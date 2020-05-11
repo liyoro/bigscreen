b2bcaa968deee05b63a9250eabd5e844946bbbbe
liyoro <liyoro@163.com>
2020-05-11 18:17:58 +0800

    20200511，first

---
+++ src/views/Home/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<style scope lang="scss">
+// @import "./index.scss";
+</style>
+
+<script>
+import indexjs from "./index.js";
+export default { ...indexjs };
+</script>
